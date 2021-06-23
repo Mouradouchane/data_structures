9a35af9669d9476e363b396bc6ae0591fb32ae72
Mourad ouchane <mouradouchane@gmail.com>
2021-06-23 07:26:59 +0100

    singly linked list push_at & clear

--- data_structures/data_structures/data_structures.cpp
+++ data_structures/data_structures/data_structures.cpp
@@ -2,28 +2,28 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+
 #include "singly_Linked_List.hpp"
 
 using namespace singlyLinkedList;
 
 int main() {
 
-    linkedList<int> firstOne;
+    singly_linkedList<int> firstOne;
 
     firstOne.push_back(14);
     firstOne.push_back(77);
     firstOne.push_back(88);
     firstOne.push_back(99);
-
-    //std::cout << "===================" << std::endl;
-
-    std::vector<int> lints = firstOne.toVector();
-    int *arr = firstOne.toArray();
     
-    for (int x = 0; x < firstOne.length(); x += 1 ) {
-        std::cout << *(arr + x) << std::endl;
-    }
+    firstOne.showAll();
 
+    firstOne.clear();
+
+    firstOne.push_back(5);
+    firstOne.push_back(9);
+
+    firstOne.showAll();
 
     return 0;
 }
--- data_structures/data_structures/singly_Linked_List.hpp
+++ data_structures/data_structures/singly_Linked_List.hpp
@@ -16,28 +16,37 @@
 
     toVector        => o(n)
     toArray         => o(n)
+
+    clear           => o(n)
+    showAll         => o(n)
 */
 
 namespace singlyLinkedList {
 
-    template<typename t> class node {
-    public:
-        node() {}
-        t value;
-        node* next = NULL;
-    };
+// ========== node ============
+template<typename t> class node {
+public:
+    node() {}
+    // only value & pointer for next one :)
+    t value;
+    node* next = NULL;
+};
 
-    template<typename t> class linkedList {
-    private : 
-        // len => length
-        int len = 0; 
+
+// ========== singly linked list ============
+template<typename t> class singly_linkedList {
+
+private : 
+        int len = 0; // len => length
         node<t>* first = NULL; // head
         node<t>* last  = NULL; // tail
-    public:
-        linkedList() {}
-        ~linkedList() {}
+
+public:
+        singly_linkedList() {}
+        ~singly_linkedList() {}
 
         // o(1)
+        // push new value direct from the back of linked list (or you can say 'new tail')
         void push_back(t value) {
             node<t>* newNode = new node<t>;
                      newNode->value = value;
@@ -56,6 +65,7 @@ namespace singlyLinkedList {
         }
 
         // o(1)
+        // push new value direct from the front of linked list (or you can say 'new head')
         void push_front(t value) {
             node<t>* newNode = new node<t>;
                      newNode->value = value;
@@ -76,38 +86,77 @@ namespace singlyLinkedList {
         }
 
         // o(n)
+        // push new value in linked list with specific position 
         void  push_at(t position , t value) {
-            len += 1;
+            len += 1; // just length of linked list :)
+
+            // new node with value 
+            node<t>* newNode = new node<t>;
+                     newNode->value = value;
+    
+            // temp for loop over all nodes & check one by one :) 
+            node<t>* temp = first;
+
+            // loop over all nodes in linked list & check for position
+            while (temp != NULL) {
+                // in case we found position 
+                if (temp->value == position) {
+                    // getting next
+                    node<t>* next = temp->next;
+                    // make next => newNode
+                    temp->next = newNode;
+                    // make newNode referring to next
+                    newNode->next = next;
+                    return; // stop as last step
+                }
+                // else keep going to next one 
+                temp = temp->next;
+            }
+
+            // in case position value not found we add newNode as last one 
+            last->next = newNode;
+            last = newNode;
         }
 
+        // o(n)
+        // just test function who print all values in console
         void showAll() {
             node<t>* tempNode = first;
 
+            std::cout << "==================================================" << std::endl;
             while (tempNode != NULL) {
                 std::cout << tempNode->value << std::endl;
                 tempNode = tempNode->next;
             }
-
+            std::cout << "==================================================" << std::endl;
         }
 
+        // o(1)
+        // get first node in linked list
         node<t> getFirstElement() {
             node<t> copy = *first;
             return copy;
         }
-
+        // o(1)
+        // get first value in first node in linked list
         t getFirstValue() {
             return first->value;
         }
 
+        // o(1)
+        // get last node in linked list
         node<t> getLastElement() {
             node<t> copy = *last;
             return copy;
         }
-
+        // o(1)
+        // get last value in last node in linked list
         t getLastValue() {
             return last->value;
         }
 
+        // o(n)
+        // method return vector of all values in linked list
         std::vector<t> toVector() {
             std::vector<t> vec;
 
@@ -121,6 +170,8 @@ namespace singlyLinkedList {
             return vec;
         }
 
+        // o(n)
+        // method return array as pointer
         t* toArray() {
             t* arr = new t[len];
             node<t>* tempNode = first;
@@ -135,9 +186,26 @@ namespace singlyLinkedList {
             return arr;
         }
 
+        // o(n)
+        // method delete all nodes in linked list 
+        void clear() {
+            node<t>* temp = first;
+
+            while (temp != NULL){
+                node<t>* next_temp = temp->next;
+                delete temp;
+                temp = next_temp;
+            }
+
+            first = NULL;
+            last = NULL;
+        }
+
+        // o(1)
+        // return length of linked size :) 
         int length() {
             return len;
         }
-    };
+};
 
 }
