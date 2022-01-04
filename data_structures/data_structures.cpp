2ad3a0293ad1bb86261d215c43b39e6d4e233e80
Mourad ouchane <mouradouchane@gmail.com>
2022-01-04 18:09:02 +0100

    some edits for binary tree

--- data_structures/data_structures.cpp
+++ data_structures/data_structures.cpp
@@ -5,41 +5,21 @@
 #include <map>
 #include <string>
 
-	// DSA 
-#include "linkedlists/singly_LinkedList.hpp"
-#include "linkedlists/doubly_LinkedList.hpp"
-#include "linkedlists/circly_singly_LinkedList.hpp"
-
-#include "stacks/dynamic_stack.hpp"
-#include "stacks/static_stack.hpp"
-
-#include "queues/static_queue.hpp"
-#include "queues/dynamic_queue.hpp"
-
-#include "hash tables/hash_table.hpp"
-#include "hash tables/hash_table_openAddressing.hpp"
-#include "hash tables/key_value_linkedlist.hpp"
-#include "hash tables/hash_table_chaining.hpp"
-
 #include "trees/dynamic_tree.hpp"
 #include "trees/binary_tree.hpp"
 
-	// using namespaces only for testing 
-using namespace linkedlist;
-using namespace stacks;
-using namespace queue;
-using namespace hash_tables;
 using namespace trees;
 
-        /*
-            ===> just cpp file for testing 
-        */
+	/*
+		===> just cpp file for testing 
+	*/
 
 int main() {
 
-	BinaryTree<int> bt(1);
+	BinaryTree<int> byt(1);
+	std::cout << "tree size ! "<< byt.length() << "\n";
+	
 
-	std::cout << "worked\n";
-
-    	return 0;
+	return 0;
 }
+
--- data_structures/trees/binary_tree.hpp
+++ data_structures/trees/binary_tree.hpp
@@ -7,21 +7,25 @@ namespace trees{
     // =======================================
     // ======= Binary tree Nodes Class =======
     // =======================================
-    template<typename type> class BT_Node{
-	  private:
-		unsigned int parent;  // parent     "index"
-            unsigned int left  ;  // left node  "index"
-           	unsigned int right ;  // right node "index"
+    template<typename t> class BT_Node{
+	  	
+		private:
+			// index's
+			unsigned int parent = NULL;	
+			// children 
+            unsigned int left;  	
+           	unsigned int right;
+
         public:
-            type value;              // node value
+            t value;
 
             // default constructor
-            BT_Node(type node_value){ 
-			value = node_value;
-		}
+            BT_Node(t node_value):value(node_value){ 
+				//value = node_value;
+			}
 
-            // destructor
-            ~BT_Node(){ }
+			// destructor
+			~BT_Node(){ }
  
     };
 
@@ -29,16 +33,63 @@ namespace trees{
 	template<typename t> class BinaryTree{
 	
 		private:
+			unsigned int len = 0;
+			unsigned int size = 0;
+			bool fixed_size = false;
+
 			// all nodes in should be here in this vector
 			std::vector<t> nodes;
 	
 		public:
-			BinaryTree(t root_value){
-				nodes[0] = root_value;
+			/*
+				=== constructors ===
+			*/
 
+			// for 'dynamic' size 
+			BinaryTree(t root_value){
+				// insert root & update length  
+				nodes.push_back(root_value);
+				len += 1;
 			}
 
+			// for 'static'  size 
+			BinaryTree(t root_value , unsigned int tree_size):fixed_size(true),size(tree_size){
+				// insert root & update length  
+				nodes.push_back(root_value);
+				len += 1;
+			}
+
+			// destructor
 			~BinaryTree(){ }
+
+			/*
+				=== methods ===
+			*/
+
+			unsigned int length(){
+				return this->len;
+			}
+
+			bool insert(t new_value){}
+
+			bool search_up(t target){}
+
+			bool search_down(t const &target){}
+
+			bool remove(t const &target){}
+
+			bool remove(unsigned int const index){}
+
+			bool move_up(){}
+
+			bool move_down(t const &target){}
+
+			bool move_down(unsigned int const &index){}
+
+			bool travle_up(){}
+
+			bool travle_down(){}
 	};
 
-};
+
+}
