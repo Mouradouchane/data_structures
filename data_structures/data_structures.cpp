83737001bd7b032037a3b1f94636d79a0c91625c
Mourad ouchane <mouradouchane@gmail.com>
2022-01-07 21:21:41 +0100

    bug in insert function in binary tree

--- data_structures/data_structures.cpp
+++ data_structures/data_structures.cpp
@@ -14,20 +14,27 @@ using namespace trees;
 		===> just cpp file for testing 
 	*/
 
-bool lmb (int &x , int &y){
+bool comp (int const &x , int const &y){
 	return x < y;
 };
 
 int main() {
 	unsigned int size = 20;
 
-	BinaryTree<int> byt( lmb );
-	BinaryTree<int> bbyt( 10, [](int &a , int &b){ return a < b; } );
-	BinaryTree<float> fbt( size , [](float &a , float &b){return a < b;} );
+	BinaryTree<int> byt( comp );
+
+	std::vector<int> x;
+
+	//if(x == 4) std::cout << "working !\n";
 
 	/*
+	byt.insert(4);
+	byt.insert(2);
+	byt.insert(1);
+	byt.insert(4);
+	byt.insert(6);
+
 	if(lmb(5,5)) std::cout << "true !!\n";
-	else std::cout << "false !!\n";
 	*/
 
 	return 0;
--- data_structures/trees/binary_tree.hpp
+++ data_structures/trees/binary_tree.hpp
@@ -36,14 +36,14 @@ namespace trees{
 	template<typename t> class BinaryTree{
 	
 		private:
-			unsigned int len = 0 , size = 0;
+			unsigned int len = 0 , max_size = 0;
 			bool fixed_size = false;
 
 			// important number represent the index of current position in tree
 			unsigned int current_index = 1;
 
 			// comapre function for searching inserting ...
-			bool(*comp_func)(t &a , t &b);
+			bool(*comp_func)(t const &a , t const &b);
 
 			// all nodes in should be here in this vector
 			std::vector<t> nodes;
@@ -54,7 +54,7 @@ namespace trees{
 			*/
 
 			// for 'dynamic' size 
-			BinaryTree( bool(*compare_function)(t &a , t &b) )
+			BinaryTree( bool(*compare_function)(t const &a , t const &b) )
 		
 					:comp_func(compare_function)
 			{
@@ -62,9 +62,9 @@ namespace trees{
 			}
 
 			// for 'static' size 
-			BinaryTree(unsigned int const tree_size , bool(*compare_function)(t &a , t &b) )
+			BinaryTree(unsigned int const tree_size , bool(*compare_function)(t const &a , t const &b) )
 				:fixed_size(true),
-				 size(tree_size),
+				 max_size(tree_size),
 				 comp_func(compare_function)
 			{
 				std::cout << "init's successed ! \n";
@@ -86,10 +86,38 @@ namespace trees{
 			// o(1)
 			// push new value to this binary tree , "index will be detected automaticlly"
 			bool insert(t const &new_value){
-				// calc index's of left/rigth child's of that parent 
-				unsigned int left  = this->current_index * 2;
-				unsigned int right = left + 1;
 
+				// check if tree is full or not that's depend on type of defintion "static" or "dynamic" size
+				if(this->fixed_size && this->len < this->max_size || !this->fixed_size ){
+					
+					// index's for searching where to insert that new value
+					unsigned int i = 0;
+
+					for(;;){
+
+						// in case value already in tree 'no duplicated allowed'
+						if(this->nodes[i] == new_value) return false;
+
+						// insert if we found empty node
+						if(this->nodes[i] == NULL ){
+							this->nodes.insert(this->nodes.begin() + i , new_value);
+							return true;
+						}
+
+						// calculate next direction left or right
+						if(this->comp_func(this->nodes[i] , new_value) ){
+							// left
+							i = (i+1) * 2;
+						}
+						else{
+							// right
+							i = ((i+1) * 2) + 1;
+						}
+					}
+				}
+
+				// else mean tree 'fixed size' & full of nodes , or that new value already exist  
+				return false;
 				
 			}
 
@@ -122,7 +150,7 @@ namespace trees{
 
 				return false;
 			}
-
+	/*
 			bool search_up(t const &target){}
 			bool search_down(t const &target){}
 
@@ -140,7 +168,7 @@ namespace trees{
 			BinaryTree<t> get_sub_tree(unsigned int const &index){}
 
 			bool is_perfect(){}
-
+	*/
 	};
 
 
