8454977b474cb95dc83afa26bafcb2f182fd6eb0
Mouradouchane <mouradouchane@gmail.com>
2022-11-16 18:50:05 +0100

    refactor:static_array.hpp
    drop:is_empty is_full get and more functions from static_array
    
    add:clear/remove/for_each based range

--- data_structures/arrays/static_array.hpp
+++ data_structures/arrays/static_array.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept> 
+
 #pragma once
 
 /*
@@ -32,16 +33,16 @@ namespace arrays {
 
 	template<typename T> class static_array {
 
+		// NOTE : "T" should not be pointer type "T*"
 		static_assert(!std::is_pointer_v<T>, "T* 'pointer type' not allowed in static_array use the alternative 'static_array_p' ");
 
 	private:
 		size_t _size = 0;
 		int len = 0;
 
-		// array its self
+		// "pointer" to the array in the heap
 		T* arr = nullptr;
-		// map : to keep track which index is empty or not
-		bool *map = nullptr;
+
 
 	public :
 
@@ -51,14 +52,12 @@ namespace arrays {
 			// save array size
 			this->_size = array_size;
 
-			// allocated array and map at the heap
+			// allocated array at the heap
 			this->arr = new T[sizeof(T) * array_size];
-			this->map = new bool[sizeof(bool) * array_size];
 
-			// o(n)
-			// declare all places in array as empty places
+			// fill with empty values
 			for (size_t i = 0; i < this->_size; i += 1) {
-				this->map[i] = false;
+				*(this->arr + i) = T();
 			}
 			
 		}
@@ -68,14 +67,13 @@ namespace arrays {
 	
 			this->_size = elements.size();
 			this->len = this->_size - 1;
+
 			this->arr = new T[sizeof(T) * this->_size];
-			this->map = new bool[sizeof(bool) * this->_size];
 
 			size_t i = 0;
 			for (T element : elements) {
 
 				*(this->arr + i) = element;
-				*(this->map + i) = true;
 
 				i += 1;
 			}
@@ -86,281 +84,303 @@ namespace arrays {
 		static_array(static_array<T> & new_array) {
 
 			this->arr = new T[sizeof(T) * new_array._size];
-			this->map = new bool[sizeof(bool) * new_array._size];
 
-			// copy process
-			
 			this->_size  = new_array._size;
 			this->len    = new_array.len;
 
+			// copy process		
 			for (size_t i = 0; i < this->_size; i += 1) {
 
 				*(this->arr + i)  = *(new_array.arr + i);
-				*(this->map + i)  = *(new_array.map + i);
 
 			}
 
 		}
 
-
-		// destructor	
+		// default destructor	
 		~static_array() { }
 
-			
-		// o(n)
-		// loop over all elements 
-		void for_each( void (* const& call_back_function)(T& element) ) {
-
-			for (size_t i = 0; i < this->_size; i += 1) {
-
-				if(this->map[i] == true) call_back_function( *(this->arr + i) );
-
-			}
-
-		}
-
-		// o(1)
-		// get the count of how many elements currently in this array
-		int length() {
-			return this->len;
-		}
-
-		// o(1)
-		// get array size
-		size_t size() {
-			return this->_size;
-		}
-
-		// o(1)
-		bool is_full() {
-			if (this->_size == 0) return false;
-			return (this->len < this->_size - 1) ? false : true;
-		}
-
-		// o(1)
-		bool is_empty() {
-			return (this->len > 0) ? false : true;
-		}
-
-		// o(n)
-		void reverse() {
-
-			// temp for swap each time
-			T temp;
-
-			// loop from the beginning to the middel and preforme swap
-			for (size_t i = 0, c = this->_size - 1; i < c ; i += 1, c -= 1) {
-
-				temp = *(this->arr + i);
-
-				*(this->arr + i) = *(this->arr + c);
-				*(this->arr + c) = temp;
-
-				temp = T();
-				/*
-				try {
-				}
-				catch (std::exception err) {
-					throw err;
-					throw ("make sure that your \"data - type\" support assignment to \"NULL\" value .");
-				}
-				*/
-			}
-
-		}
-
-		// o(n)
-		// replace all elements empty values
-		void clear() {
-
-			for (size_t i = 0; i < this->_size; i += 1) *(this->arr + i) = T();
-
-		}
-		
-		// o(1)
-		bool insert_at(size_t const& target_index , const T& new_element ) {
-
-			// if index out of array range
-			if (target_index >= this->_size) return false;
-			else {
-
-				// otherwise insert
-				*(this->arr + target_index) = new_element;
-
-				this->map[target_index] = true;
-				this->len += 1;
-
-				return true;
-			}
-
-		}
-
-		// o(1)
-		// remove element from array 
-		// NOTE : this function it's not affect the size of array 
-		// NOTE : remove "just as declare" the element will be there until overwrite or the hole array get deleted 
-		bool remove( size_t const& target_index ) {
-
-			// if index out or range
-			if ( target_index >= this->_size ) return false;
-
-			// if already empty
-			if (this->map[target_index] == false) return false;
-
-			// declare as removed
-			this->map[target_index] = false;
-			this->len -= 1;
-
-			return true;
-		}
-		
-		// o(range)
-		// "virtual remove" range of elements in array
-		bool remove(size_t const& start_index, size_t const& end_index) {
-
-			if (start_index < 0 || start_index >= this->_size) return false;
-			if (end_index   < 0 || end_index   >= this->_size) return false;
-			if (end_index < start_index) return false;
-
-			for (size_t i = start_index; i <= end_index; i += 1) {
-				this->remove(i);
-			}
-
-			return true;
-		}
-
-		// o(1)
-		// "get element by value"
-		T get(size_t const& index) {
-
-			if (index >= this->_size) return *(this->arr + this->_size);
-
-			if (this->map[index] == false) return *(this->arr + this->_size);
-			
-			else return *(this->arr + index);
-		}
-
-
 		/*
-		
-			-------------- public operators --------------
-
+			===== methods =====
 		*/
 
-		void operator = (std::initializer_list<T> const& elements) {
-
-			this->_size = elements.size();
-			this->len = this->_size - 1;
-			this->arr = new T[sizeof(T) * this->_size];
-			this->map = new bool[sizeof(bool) * this->_size];
-
-			size_t i = 0;
-			for (T element : elements) {
-
-				*(this->arr + i) = element;
-				*(this->map + i) = true;
-
-				i += 1;
-			}
-
-		}
-
-		T& operator[] (size_t const& index) {
-			return *(this->arr + index);
-		}
-
-
-		// O(1)
-
-		T* begin() {
-			return (this->arr + 0);
-		}
-		T* cbegin() {
-			return (this->arr + this->_size - 1);
-		}
-
-		T* end() {
-			// "size + 1" for "out of range" address
-			return  (this->arr + this->_size);
-		}
-		T* cend() {
-			return  (this->arr - 1);
-		}
-
+		void for_each(void (* const& call_back_function)(T& element)); // O(n)
+		void for_each(
+			size_t const& start_index , 
+			size_t const& end_index ,
+			void (* const& call_back_function)(T& element)
+		); // O(n)
+		size_t length();// O(1)
+		size_t size();	// O(1)
+		void reverse(); // O(n)
+		void clear();	// O(n)
+		void clear(size_t const& start_index, size_t const& end_index); // O(range)
+		bool insert_at(size_t const& target_index, T const& new_element); // O(n)
+		bool remove(size_t const& target_index); // O(n)
+		bool remove(size_t const& start_index, size_t const& end_index); // O(range)
 
 		/*
-
-			-------------- iterators --------------
-
+			===== methods for iterators =====
 		*/
-
-		class iterator {
-
-			private : 
-				T* addr = nullptr;
-				int index = 0;
-
-			public:
-				iterator() { }
-				iterator(T* address) :addr{ address } { }
-			
-				// access and refer operators
-				T operator *() {
-					return *(this->addr);
-				}
-				T* operator &() {
-					return this->addr;
-				}
-
-				// increment operators
-				void operator ++() {
-					this->addr = (this->addr + 1);
-				}
-				void operator +=( int const& increment_value ) {
-					this->addr = (this->addr + increment_value);
-				}
-
-				// decrement operators
-				void operator --() {
-					this->addr = (this->addr - 1);
-				}
-				void operator -= ( int const& decrement_value ) {
-					this->addr = (this->addr - decrement_value);
-				}
-
-				T* operator -> () {
-					return this->addr;
-				}
-
-				// =========== compare iterator to reference ===========
-				bool operator != ( T * address_of_other_element ) {
-					return (this->addr != address_of_other_element) ? true : false;
-				}
-
-				bool operator == (T* address_of_other_element) {
-					return (this->addr == address_of_other_element) ? true : false;
-				}
-
-
-				// =========== compare iterator to iterator ===========
-				bool operator != (static_array<T>::iterator & other_iterator) {
-					return (this->addr != other_iterator.addr) ? true : false;
-				}
-
-				bool operator == (static_array<T>::iterator& other_iterator) {
-					return (this->addr == other_iterator.addr) ? true : false;
-				}
-
-		}; // end of class iterator
-
+		T* begin(); // O(1)
+		T* cbegin(); // O(1)
+		T* end(); // O(1)
+		T* cend(); // O(1)
 
 		/*
-			-------------- methods for iterators --------------
+			===== operators =====
 		*/
+		void operator = (std::initializer_list<T> const& elements); // O(1)
+		T& operator[] (size_t const& index); // O(1)
+
+		/*
+			===== iterators =====
+		*/
+		class iterator;
+
+	}; 
+	/*
+
+		end of class static_array
+
+	*/
 
 
-	}; // end of class static_array
 
+	/*
+		-------------- static_array public methods --------------
+	*/
 
+	// loop over all elements
+	template<typename T> void static_array<T>::for_each(void (* const& call_back_function)(T& element)) {
+
+		for (size_t i = 0; i < this->_size; i += 1) {
+
+			call_back_function( *(this->arr + i) );
+
+		}
+
+	}
+
+	template<typename T> void static_array<T>::for_each(
+		size_t const& start_index, 
+		size_t const& end_index, 
+		void (* const& call_back_function)(T& element)
+	) {
+
+		if (start_index >= this->_size) return;
+		if (end_index >= this->_size) return;
+		if (start_index > end_index) return;
+
+		for (size_t i = start_index; i <= end_index; i += 1) {
+
+			call_back_function( *(this->arr + i) );
+
+		}
+	}
+
+	// size - 1
+	template<typename T> size_t static_array<T>::length() {
+		return (size_t)this->len;
+	}
+
+	// array size
+	template<typename T> size_t static_array<T>::size() {
+		return this->_size;
+	}
+
+	template<typename T> void static_array<T>::reverse() {
+
+		T temp ; // for swaping elements
+
+		// loop from the beginning to the middel and preforme swap
+		for (size_t i = 0, c = this->_size - 1; i < c; i += 1, c -= 1) {
+
+			temp = *(this->arr + i);
+
+			*(this->arr + i) = *(this->arr + c);
+			*(this->arr + c) = temp;
+
+			temp = T();
 	
+		}
+
+	}
+
+	// try to replace all elements with empty value
+	template<typename T> void static_array<T>::clear() {
+
+		for (size_t i = 0; i < this->_size; i += 1) *(this->arr + i) = T();
+
+	}
+
+	template<typename T> void static_array<T>::clear(size_t const& start_index, size_t const& end_index) {
+
+		if (start_index >= this->_size) return;
+		if (end_index >= this->_size) return;
+
+		if (start_index > end_index) return;
+
+		for (size_t i = start_index; i <= end_index; i += 1) {
+
+			*(this->arr + i) = T();
+
+		}
+
+	}
+
+	template<typename T> bool static_array<T>::insert_at(size_t const& target_index, T const& new_element) {
+
+		// if index out of array range
+		if (target_index >= this->_size) return false;
+		else {
+			// otherwise insert
+
+			*(this->arr + target_index) = new_element;
+
+			this->len += 1;
+
+			return true;
+		}
+
+	}
+
+	// remove => repalce element with "T()" value
+	template<typename T> bool static_array<T>::remove(size_t const& target_index) {
+
+		// if index out or range
+		if (target_index >= this->_size) return false;
+		
+		// else
+		*(this->arr + target_index) = T();
+		this->len -= 1;
+
+		return true;
+	}
+
+	// replace a range of elements in array with "T()"
+	template<typename T> bool static_array<T>::remove(size_t const& start_index, size_t const& end_index) {
+
+		// if out of range
+		if (start_index >= this->_size) return false;
+		if (end_index >= this->_size) return false;
+
+		// no reversed ranges :)
+		if (end_index < start_index) return false;
+
+		for (size_t i = start_index; i <= end_index; i += 1) {
+			this->remove(i);
+		}
+
+		return true;
+	}
+
+
+	/*
+		-------------- static_array public operators --------------
+	*/
+	template<typename T> void static_array<T>::operator = (std::initializer_list<T> const& elements) {
+
+		this->_size = elements.size();
+		this->len = this->_size - 1;
+		this->arr = new T[sizeof(T) * this->_size];
+
+		size_t i = 0;
+		for (T element : elements) {
+
+			*(this->arr + i) = element;
+
+			i += 1;
+		}
+
+	}
+	template<typename T> T& static_array<T>::operator[] (size_t const& index) {
+		return *(this->arr + index);
+	}
+
+
+	/*
+		-------------- static_array methods for iterators --------------
+	*/
+	template<typename T> T* static_array<T>::begin() {
+		return (this->arr + 0);
+	}
+	template<typename T> T* static_array<T>::cbegin() {
+		return (this->arr + this->_size - 1);
+	}
+
+	template<typename T> T* static_array<T>::end() {
+		// "size + 1" for "out of range" address
+		return  (this->arr + this->_size);
+	}
+	template<typename T> T* static_array<T>::cend() {
+		// something like -1 index
+		return  (this->arr - 1);
+	}
+
+
+	/*
+		-------------- static_array iterator class --------------
+	*/
+	template<typename T> class static_array<T>::iterator {
+
+		private:
+			T* addr = nullptr;
+			int index = 0;
+
+		public:
+			iterator() { }
+			iterator(T* address) :addr{ address } { }
+
+			// access and refer operators
+			T operator *() {
+				return *(this->addr);
+			}
+			T* operator &() {
+				return this->addr;
+			}
+
+			// increment operators
+			void operator ++() {
+				this->addr = (this->addr + 1);
+			}
+			void operator +=(int const& increment_value) {
+				this->addr = (this->addr + increment_value);
+			}
+
+			// decrement operators
+			void operator --() {
+				this->addr = (this->addr - 1);
+			}
+			void operator -= (int const& decrement_value) {
+				this->addr = (this->addr - decrement_value);
+			}
+
+			T* operator -> () {
+				return this->addr;
+			}
+
+			// =========== compare iterator to reference ===========
+			bool operator != (T* address_of_other_element) {
+				return (this->addr != address_of_other_element) ? true : false;
+			}
+
+			bool operator == (T* address_of_other_element) {
+				return (this->addr == address_of_other_element) ? true : false;
+			}
+
+
+			// =========== compare iterator to iterator ===========
+			bool operator != (static_array<T>::iterator& other_iterator) {
+				return (this->addr != other_iterator.addr) ? true : false;
+			}
+
+			bool operator == (static_array<T>::iterator& other_iterator) {
+				return (this->addr == other_iterator.addr) ? true : false;
+			}
+
+	}; 
+	// end of class iterator
+
+
 } // end of namespace arrays
--- data_structures/data_structures.cpp
+++ data_structures/data_structures.cpp
@@ -5,29 +5,30 @@
 #include <algorithm>
 #include <map>
 #include <string>
+// #include <functional>
 
 #include "arrays/static_array.hpp"
 #include "arrays/dynamic_array.hpp"
 
+#define op ==
 
 class pdf;
 void test1();
 void test2();
 
-#define op ==
-
 int main() {
 
 	/*
+	*/
 	std::cout << "=============== OBJECTS TEST ===============\n";
 	test1();
-	*/
 	std::cout << "=============== PRIMITIVE TEST ===============\n";
 	test2();
 
 	return 0;
 
 }
+
 
 // objects for testing 
 class pdf {
@@ -57,35 +58,24 @@ public:
 void test1() {
 
 	arrays::static_array<pdf> arr = {
-		pdf("pdf1") , pdf("pdf2") , pdf("pdf3")
+		pdf("pdf1") , pdf("pdf2") , pdf("pdf3") , pdf("pdf4")
 	};
 
 	for (pdf &p : arr) {
 		std::cout << p.data << '\n';
 	}
 
-	/*
-	for (arrays::static_array<pdf>::iterator i = &arr[0]; i != arr.end() ; i += 1 ) {
-		std::cout << "itr : " << i->data << '\n';
-	}
-	*/
-
-
-	std::cout << "================================" << '\n';
+	std::cout << "===============================================" << '\n';
 	std::cout << "SIZE    : " << arr.size() << '\n';
 	std::cout << "LENGTH  : " << arr.length() << '\n';
-	std::cout << "IS_EMPTY: " << arr.is_empty() << '\n';
-	std::cout << "IS_FULL : " << arr.is_full() << '\n';
-	std::cout << "================================" << '\n';
+	std::cout << "===============================================" << '\n';
 
 }
 
 // test on primitives "int float ..."
 void test2() {
 
-	arrays::static_array<int> arr  = { 1, 1, 3 , 6, 2, 9 };
-
-	arr[2] = 55;
+	arrays::static_array<int> arr = { 1, 1, 3 , 6, 2, 9 };
 
 	for (int& num : arr) {
 
@@ -93,11 +83,9 @@ void test2() {
 
 	}
 
-	std::cout << "================================" << '\n';
+	std::cout << "===============================================" << '\n';
 	std::cout << "SIZE    : " << arr.size() << '\n';
 	std::cout << "LENGTH  : " << arr.length() << '\n';
-	std::cout << "IS_EMPTY: " << arr.is_empty() << '\n';
-	std::cout << "IS_FULL : " << arr.is_full() << '\n';
-	std::cout << "================================" << '\n';
+	std::cout << "===============================================" << '\n';
 
 }
