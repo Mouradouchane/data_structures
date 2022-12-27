c04a74486cf2c16c14568ce945b8867e1481b4b3
0x00001F <34870188+Mouradouchane@users.noreply.github.com>
2022-12-27 12:25:51 +0100

    rewrite bases in list based graph

--- data_structures/graphs/graph_list.hpp
+++ data_structures/graphs/graph_list.hpp
@@ -6,141 +6,98 @@
 
 #pragma once 
 
+
 namespace graphs {
-	
-	// template<typename type> class l_graph;
 
-	template<typename T> class vertex {
+template<typename t> class vertex{
 
-		private:
-			std::set< vertex<T>& > edges; // this vertex connection
+	private:
+		std::set< vertex<t>* > edges;	
+		std::string name = "";
 
-		public:
-			std::string name;
-			T value;
+	public :
+		t value;
 
-			// def constructor
-			vertex() {}
+	vertex(){ }
 
-			// constructor 1
-			vertex(std::string vertex_name, T vertex_value)
-				: name(vertex_name) , value(vertex_value)
-			{
+	vertex(
 
-			}
+		std::string vertex_name , 
+		t vertex_value , 
+		std::set<vertex<t>*> const& vertex_edges = {}
 
-			// constructor 2
-			vertex(T vertex_value) : value(vertex_value) { }
+	)	:name(vertex_name), value(vertex_value) {
 
-			// constructor 3
-			vertex(T vertex_value , std::initializer_list< vertex<T>& > const& vertex_edges ) 
-				: value(vertex_value) 
-			{ 
-
-				for (vertex<T> & other : vertex_edges) {
-					this->edges.insert(other);
-					other.edges.insert(*this);
-				}
-
-			}
-
-			// constructor 4
-			vertex(std::string vertex_name, T vertex_value, std::initializer_list< vertex<T>& > const& vertex_edges)
-				: name(vertex_name) , value(vertex_value)
-			{
-
-				for (vertex<T>& other : vertex_edges) {
-					this->edges.insert(other);
-					other.edges.insert(*this);
-				}
-
-			}
-
-			// destructor 
-			~vertex() { }
-
-			template<typename T> friend class l_graph;
-	};
-	// end of class vertex
-
-
-	/*
-		- graph based on list
-		- undirected graph
-		- dynamic graph
-	*/
-	template<typename type> class l_graph {
-
-		private :
-			std::set< vertex<type> > graph;
-
-			//bool (* compare_function)(type const& vertex_a_value, type const& vertex_b_value);
-			std::function< bool(type const& a, type const& b) > cp;
-
-		public :
-
-			// constructor's
-
-			l_graph( 
-				//bool (* comparison_function)(type const& vertex_a_value, type const& vertex_b_value) 
-				std::function< bool(type const& a, type const& b) > comparison_function
-			)
-				//:compare_function(comparison_function)
-			{ 
-				this->cp = comparison_function;
-			}
-
-			l_graph( std::initializer_list<type> const& values ) {
-			
-				// copy process
-				for (type value : values) {
-					this->graph.insert( vertex<type>(value) );
-				}
-
-			}
-
-			~l_graph(){}
-
-
-			// graph method's
-
-			void add_vertex(type vertex_value);
-			void add_vertex(std::string vertex_name , type vertex_value);
-			void add_vertex(vertex<type> const& vertex_value);
-			void add_vertex(type& vertex_value, std::initializer_list< vertex<type>& > const& vertex_edges);
-			void add_edge(type const& vertex_a, type const& vertex_b); // to connect to vertex's 
-
-			void delete_vertex(	type const& target_vertex);
-			void delete_vertex(	size_t const& vertex_index);
-			void delete_edge(type const& vertex_a, type const& vertex_b); // to delete connection between vertex's
-
-			void bfs(); // search algorithm !
-			void dfs(); // search algorithm !
-
-			// for testing 
-			void print();
-
-	}; // end of class l_graph
-
-	 template<typename type> void l_graph<type>::add_vertex(type vertex_value) {
-		
-		this->graph.insert( vertex<type>(vertex_value) );
-
-	}
-
-	template<typename type> void l_graph<type>::print() {
-
-		for (vertex<type> & vertex : this->graph) {
-			std::cout << vertex.name << " : " << vertex.value  << ":: [";
-			/*
-			for ( vertex<T>& edge : vertex.edges)  {
-				std::cout << edge.value << " , ";
-			}
-			*/
-
-			std::cout << "]\n";
+		// copy edges
+		for( vertex<t>* edge : vertex_edges ) {
+			this->edges.insert( edge );			
 		}
 
 	}
 
-} // end of namespace graphs
+
+	~vertex(){ }
+
+	// getters
+	std::string get_name();
+
+	vertex<t>* go_to( size_t const& vertex_index );
+	vertex<t>* go_to( std::string const& vertex_name );
+
+} // end of class vertex
+
+
+
+template<typename t> class l_graph{
+
+	private :
+		std::set< vertex<t> > graph; // graph vertices
+
+	public :
+
+		vertex<t> * vertex = nullptr;
+
+		l_graph(){ }
+
+		l_graph(std::set< vertex<t> > const& vertices = {} ) {
+
+			// copy vertices to the graph
+			for( vertex<t> vertex : vertices ) {
+				this->graph.insert( vertex );
+			}
+
+		}	
+
+	bool add_vertex( 
+		std::string vertex_name , 
+		t vertex_value , 
+		std::set<size_t> const& edges_indexes = {} 
+	);
+
+	bool add_vertex( 
+		std::string vertex_name , 
+		t vertex_value , 
+		std::set< vertex<t>* > const& edges_pointers = {} 
+	);
+
+	bool add_edge( size_t const& vertex_a_index , size_t const& vertex_b_index );
+	bool add_edge( std::string const& vertex_a_name , std::string const& vertex_b_name);
+
+	bool remove_vertex( size_t const& vertex_index );
+	bool remove_vertex( std::string const& vertex_name );
+
+	bool remove_edge( size_t const& vertex_a_index , size_t const& vertex_b_index );
+	bool remove_edge( std::string const& vertex_a_name , std::string const& vertex_b_name );
+
+	int search_by_name( std::string const& vertex_name );
+
+	bool is_connected( vertex<t>* const& vertex_a , vertex<t>* const& vertex_b );
+	bool is_connected( size_t const& vertex_a_index , size_t const& vertex_b_index );
+
+
+} // end of class l_graph 
+
+
+} // end of namespace graphs 
+
+
